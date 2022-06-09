63fbe524546fc6b89ff094c7618c03b6b0b42c84
Siarhei Khonski <armonius@mail.ru>
2022-06-09 13:17:33 +0300

    refactor: refactor the up method

--- src/modules/navigation/up.mjs
+++ src/modules/navigation/up.mjs
@@ -1,5 +1,6 @@
 import path from "path";
 
 export default function() {
-  this.workDir = this.workDir.split(path.sep).pop().length === 0 ? this.workDir : this.workDir.split(path.sep).slice(0, -1).join(path.sep);
+  //this.workDir = this.workDir.split(path.sep).pop().length === 0 ? this.workDir : this.workDir.split(path.sep).slice(0, -1).join(path.sep);
+  this.workDir = path.join(this.workDir, "..");
 };
