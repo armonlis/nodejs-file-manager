8d338781a6a18d33decc0f3985a396947fc50455
Siarhei Khonski <armonius@mail.ru>
2022-06-12 10:16:14 +0300

    fix: fix cd method

--- src/modules/navigation/cd.mjs
+++ src/modules/navigation/cd.mjs
@@ -1,14 +1,16 @@
 import path from "path";
-import { access } from "fs/promises";
+import { stat } from "fs/promises";
 import { constants } from "fs";
 
 export default async function(newPath = "") {
   if (path.isAbsolute(newPath)) {
-    await access(newPath, constants.F_OK);
+    const status = await stat(newPath);
+    if (status.isFile()) { throw new Error(`${newPath} is a file`) }
     this.workDir = newPath;   
   } else {
     const joinedPath = path.join(this.workDir, newPath);
-    await access(joinedPath, constants.F_OK);
+    const status = await stat(joinedPath);
+    if (status.isFile()) { throw new Error(`${newPath} is a file`) }
     this.workDir = joinedPath;
   }
 };
